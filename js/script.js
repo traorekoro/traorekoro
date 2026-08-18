86521550501601a7da982861659f594f343beea7
traorekoro <korotoumoutraore56@gmail.com>
2026-08-18 21:56:35 +0000

    executive support features

--- js/script.js
+++ js/script.js
@@ -5,50 +5,46 @@
  * GITHUB: #!
  */
 
-
-
 (function ($) {
-    'use strict';
-    
-AOS.init({
-    once: true
-});
+  "use strict";
 
-    // ----------------------- 
-        // Progress Bar--------------------
-        // 
-        // 
+  AOS.init({
+    once: true,
+  });
 
-    $(window).on ('load', function (){ 
-          
-        $('.progress-bar').each(function(){
-                var width = $(this).data('percent');
-                $(this).css({'transition': 'width 3s'});
-                $(this).appear(function() {
-                    console.log('hello');
-                    $(this).css('width', width + '%');
-                    $(this).find('.count').countTo({
-                        from: 0,
-                        to: width,
-                        speed: 3000,
-                        refreshInterval: 50
-                    });
-                });
-            });
-        }); 
-// Existing Owl Carousel
-$('.owl-carousel')
-    .not('.tech-carousel, .social-carousel, .branding-carousel')
-    .owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:8000,
-        dots:false
+  // -----------------------
+  // Progress Bar--------------------
+  //
+  //
+
+  $(window).on("load", function () {
+    $(".progress-bar").each(function () {
+      var width = $(this).data("percent");
+      $(this).css({ transition: "width 3s" });
+      $(this).appear(function () {
+        console.log("hello");
+        $(this).css("width", width + "%");
+        $(this).find(".count").countTo({
+          from: 0,
+          to: width,
+          speed: 3000,
+          refreshInterval: 50,
+        });
+      });
     });
-// Instagram Preview
-$('.tech-carousel, .social-carousel, .branding-carousel').owlCarousel({
-
+  });
+  // Existing Owl Carousel
+  $(".owl-carousel")
+    .not(".tech-carousel, .social-carousel, .branding-carousel")
+    .owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 8000,
+      dots: false,
+    });
+  // Instagram Preview
+  $(".tech-carousel, .social-carousel, .branding-carousel").owlCarousel({
     items: 1,
     loop: true,
     margin: 0,
@@ -59,53 +55,52 @@ $('.tech-carousel, .social-carousel, .branding-carousel').owlCarousel({
     dots: true,
     nav: false,
     mouseDrag: true,
-    touchDrag: true
-
-});
-/* ==========================================
+    touchDrag: true,
+  });
+  /* ==========================================
    Instagram Navigation Arrows
 ========================================== */
 
-$('.instagram-card').each(function () {
+  $(".instagram-card").each(function () {
+    var owl = $(this).find(".owl-carousel");
 
-    var owl = $(this).find('.owl-carousel');
+    $(this)
+      .find(".next")
+      .on("click", function () {
+        owl.trigger("next.owl.carousel");
+      });
 
-    $(this).find('.next').on('click', function () {
-        owl.trigger('next.owl.carousel');
-    });
+    $(this)
+      .find(".prev")
+      .on("click", function () {
+        owl.trigger("prev.owl.carousel");
+      });
+  });
 
-    $(this).find('.prev').on('click', function () {
-        owl.trigger('prev.owl.carousel');
-    });
+  // Shuffle js filter and masonry
+  var Shuffle = window.Shuffle;
+  var jQuery = window.jQuery;
 
-});
+  var myShuffle = new Shuffle(document.querySelector(".shuffle-wrapper"), {
+    itemSelector: ".shuffle-item",
+    buffer: 1,
+  });
 
+  jQuery('input[name="shuffle-filter"]').on("change", function (evt) {
+    var input = evt.currentTarget;
+    if (input.checked) {
+      myShuffle.filter(input.value);
+    }
+  });
 
-
-    // Shuffle js filter and masonry
-    var Shuffle = window.Shuffle;
-    var jQuery = window.jQuery;
-
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
-
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
-
-     $('.portfolio-gallery').each(function () {
-        $(this).find('.popup-gallery').magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    });
-
-
+  $(".portfolio-gallery").each(function () {
+    $(this)
+      .find(".popup-gallery")
+      .magnificPopup({
+        type: "image",
+        gallery: {
+          enabled: true,
+        },
+      });
+  });
 })(jQuery);
